e4ab30f7b43dc479df1ed0cf003158dd14efc433
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-06-23 14:24:45 +0530

    Add solution: 1. Two Sum (Java)

---
+++ problems/0001-Two-Sum/solution.java
@@ -0,0 +1,18 @@
+// 1. Two Sum
+// Difficulty: Easy
+// https://leetcode.com/problems/two-sum/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> aa=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int x=target-nums[i];
+            if(aa.containsKey(x)){
+                return new int[]{i,aa.get(x)};
+            }
+            else{
+                aa.put(nums[i],i);
+            }
+        }
+        return new int[]{-1,-1};
+    }
