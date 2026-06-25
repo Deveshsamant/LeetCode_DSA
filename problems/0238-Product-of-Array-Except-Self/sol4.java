32555f8222ab4c069dd03d71d3660d1611e1ff69
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-06-25 12:42:57 +0530

    Add sol4: 238. Product of Array Except Self (Java)

---
+++ problems/0238-Product-of-Array-Except-Self/sol4.java
@@ -0,0 +1,24 @@
+// ==========================================================
+// 238. Product of Array Except Self
+// Difficulty : Medium
+// Language   : Java
+// Solution   : #4
+// Runtime    : 2 ms (Beats 95%)
+// Memory     : 71.9 MB (Beats 58%)
+// Link       : https://leetcode.com/problems/product-of-array-except-self/
+// ==========================================================
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+    int n = nums.length;
+    int[] res = new int[n];
+    res[0] = 1;
+    for (int i = 1; i < n; i++) {
+        res[i] = res[i-1] * nums[i-1];
+    }
+
+    int suffix = 1;
+    for (int i = n - 1; i >= 0; i--) {
+        res[i] *= suffix;
+        suffix *= nums[i];
+    }
