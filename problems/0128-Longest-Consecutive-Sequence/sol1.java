eac37c559bd67b7bf22d0135be8fe8e9d036c744
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-06-27 09:31:03 +0530

    Add sol1: 128. Longest Consecutive Sequence (Java)

---
+++ problems/0128-Longest-Consecutive-Sequence/sol1.java
@@ -0,0 +1,24 @@
+// ==========================================================
+// 128. Longest Consecutive Sequence
+// Difficulty : Medium
+// Language   : Java
+// Solution   : #1
+// Runtime    : 33 ms (Beats 27%)
+// Memory     : 95.7 MB (Beats 40%)
+// Link       : https://leetcode.com/problems/longest-consecutive-sequence/
+// ==========================================================
+
+                int current = i;
+                while (aa.contains(current + 1)) {
+                    current++;
+                int count = 1;
+            if (!aa.contains(i - 1)) {
+        int maxseq = 0;
+        for (int i : aa) {
+
+        }
+            aa.add(i);
+        for (int i : nums) {
+        Set<Integer> aa = new HashSet<>();
+    public int longestConsecutive(int[] nums) {
+class Solution {
