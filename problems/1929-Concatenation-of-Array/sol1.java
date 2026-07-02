0d920d1f8b688f0fbbd16517da45e7e6920e15e4
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-07-02 11:11:23 +0530

    Add sol1: 1929. Concatenation of Array (Java)

---
+++ problems/1929-Concatenation-of-Array/sol1.java
@@ -0,0 +1,11 @@
+// ==========================================================
+// 1929. Concatenation of Array
+// Difficulty : Easy
+// Language   : Java
+// Solution   : #1
+// Runtime    : 2 ms (Beats 8%)
+// Memory     : 47.1 MB (Beats 76%)
+// Link       : https://leetcode.com/problems/concatenation-of-array/
+// ==========================================================
+
+ 
