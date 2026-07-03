cf4b6fac4d1542f04925a69f544e56e08cb72883
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-07-03 09:19:27 +0530

    Add sol1: 27. Remove Element (Java)

---
+++ problems/0027-Remove-Element/sol1.java
@@ -0,0 +1,11 @@
+// ==========================================================
+// 27. Remove Element
+// Difficulty : Easy
+// Language   : Java
+// Solution   : #1
+// Runtime    : 0 ms (Beats 100%)
+// Memory     : 43.4 MB (Beats 79%)
+// Link       : https://leetcode.com/problems/remove-element/
+// ==========================================================
+
+ 
