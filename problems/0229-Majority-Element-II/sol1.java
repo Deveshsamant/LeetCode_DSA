460840d56f97d682d795c42fcfd2bf25252531d1
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-01 22:07:25 +0530

    Add sol1: 229. Majority Element II (Java)

---
+++ problems/0229-Majority-Element-II/sol1.java
@@ -0,0 +1,24 @@
+// ==========================================================
+// 229. Majority Element II
+// Difficulty : Medium
+// Language   : Java
+// Solution   : #1
+// Runtime    : 3 ms (Beats 73%)
+// Memory     : 52.7 MB (Beats 41%)
+// Link       : https://leetcode.com/problems/majority-element-ii/
+// ==========================================================
+
+           else if(count1==0){
+           }
+                count2++;
+           else if(nums[i]==candidate2 && candidate1!=nums[i]){
+           }
+                count1++;
+           if(nums[i]==candidate1 && candidate2!=nums[i]){
+        for(int i=0;i<nums.length;i++){
+        int candidate2=Integer.MIN_VALUE;
+                candidate1=nums[i];
+                count1=1;
+           }
+           else if(count2==0){
+                candidate2=nums[i];
