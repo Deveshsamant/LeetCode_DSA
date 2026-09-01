b9f924d58d041db96cd7266d5be9a0628b4de7a3
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-01 12:12:02 +0530

    Add sol1: 74. Search a 2D Matrix (Java)

---
+++ problems/0074-Search-a-2D-Matrix/sol1.java
@@ -0,0 +1,24 @@
+// ==========================================================
+// 74. Search a 2D Matrix
+// Difficulty : Medium
+// Language   : Java
+// Solution   : #1
+// Runtime    : 0 ms (Beats 100%)
+// Memory     : 44.1 MB (Beats 9%)
+// Link       : https://leetcode.com/problems/search-a-2d-matrix/
+// ==========================================================
+
+            else if(matrix[i][j]>target){
+            }
+                return true;
+            if(matrix[i][j]==target){
+            int j=mid%matrix[0].length;
+                r=mid-1;
+            }
+            int i=mid/matrix[0].length;
+            int mid=l+((r-l)/2);
+        while(l<=r){
+        int r=(matrix.length*matrix[0].length)-1;
+        int l=0;
+            else{
+                l=mid+1;
