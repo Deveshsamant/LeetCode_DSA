58a1f9c6f7929234c950f0a19fe02d3ac7225993
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-02 19:04:54 +0530

    Add sol2: 62. Unique Paths (Java)

---
+++ problems/0062-Unique-Paths/sol2.java
@@ -0,0 +1,21 @@
+// ==========================================================
+// 62. Unique Paths
+// Difficulty : Medium
+// Language   : Java
+// Solution   : #2
+// Runtime    : 0 ms (Beats 100%)
+// Memory     : 42 MB (Beats 82%)
+// Link       : https://leetcode.com/problems/unique-paths/
+// ==========================================================
+
+        }
+            result=result*(a -b+i)/i;
+        for (long i = 1; i <= b; i++) {
+        long result = 1;
+        long b = Math.min(m - 1, n - 1);
+        long a = m + n - 2;
+    public int uniquePaths(int m, int n) {
+class Solution {
+        return (int) result;
+    }
+}
