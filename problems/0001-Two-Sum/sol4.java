cfd2e48f1ec26b6a9bd8d43c359d2219fefcd0ed
Devesh Samant <96580822+Deveshsamant@users.noreply.github.com>
2026-09-04 18:10:13 +0530

    Add sol4: 1. Two Sum (Java)

---
+++ problems/0001-Two-Sum/sol4.java
@@ -0,0 +1,12 @@
+// ==========================================================
+// 1. Two Sum
+// Difficulty : Easy
+// Language   : Java
+// Solution   : #4
+// Runtime    : 2 ms (Beats 99%)
+// Memory     : 47 MB (Beats 59%)
+// Link       : https://leetcode.com/problems/two-sum/
+// ==========================================================
+
+ 
+ 
